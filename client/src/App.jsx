55935749759a2295cd5a57827a17b67a3099746e
Victor Motta <victormotta7@gmail.com>
2022-02-22 17:02:17 -0300

    main cards

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,6 +8,22 @@ function App() {
   return (
     <div>
       <Navbar />
+
+      <div className="main__container">
+        <div className="main__cards">
+          <div className="main__card-todo custom__card">
+            <h2>To do</h2>
+          </div>
+          <div className="main__card-doing custom__card">
+            <h2>Doing</h2>
+            <ul className="main__card-doing_list"></ul>
+          </div>
+          <div className="main__card-done custom__card">
+            <h2>Done</h2>
+            <ul className="main__card-done_list"></ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
