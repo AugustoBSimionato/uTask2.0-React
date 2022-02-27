379d996d302ff9ddae2651fdbe92297f6440cabc
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2022-02-27 14:44:54 -0300

    a

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ const CONNECTION_URL = 'mongodb+srv://AugustoBS:K5z4SKFnpFLvC24@clusterutask.o0o
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log('Server rodando na porta: ${PORT}')))
+  .then(() => app.listen(PORT, () => console.log(`Server rodando na porta: ${PORT}`)))
   .catch((error) => console.log(error.messagem));
 
 mongoose.set('useFindAndModify', false);
