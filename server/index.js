a1ec34e60f65edd402f879be1ee330179c35bcbb
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2022-02-27 11:15:32 -0300

    confirm

--- server/index.js
+++ server/index.js
@@ -9,4 +9,6 @@ app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
+const CONNECTION_URL = 'mongodb+srv://AugustoBS:<password>@clusterutask.o0ouc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
 // https://www.mongodb.com/cloud/atlas
