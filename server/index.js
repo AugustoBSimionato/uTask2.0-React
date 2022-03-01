0c09f1d942d6c98b772fee35e4e1d4fc3334ae5b
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2022-03-01 17:49:15 -0300

    adding and reviewing the backend

--- server/index.js
+++ server/index.js
@@ -1,23 +1,27 @@
-import express from 'express';
-import bodyParser from 'body-parser';
-import mongoose from 'mongoose';
-import cors from 'cors';
+import express from "express";
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+import cors from "cors";
 
-import postRoutes from './routes/posts.js';
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
-app.use('/posts', postRoutes);
+app.use("/posts", postRoutes);
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
-const CONNECTION_URL = 'mongodb+srv://AugustoBS:K5z4SKFnpFLvC24@clusterutask.o0ouc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+const CONNECTION_URL =
+  "mongodb+srv://root:admin@clusterutask.o0ouc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server rodando na porta: ${PORT}`)))
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () => console.log(`Server running on port: ${PORT}`))
+  )
   .catch((error) => console.log(error.messagem));
 
-mongoose.set('useFindAndModify', false);
+mongoose.set("useFindAndModify", false);
