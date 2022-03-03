ff5112d9b7baeea23c95d9a4c77df0f9e99b5011
Augusto Baroni Simionato <72254418+AugustoBSimionato@users.noreply.github.com>
2022-03-03 10:11:00 -0300

    adding MongoDB user and password

--- server/index.js
+++ server/index.js
@@ -13,7 +13,7 @@ app.use(cors());
 app.use('/posts', postRoutes);
 
 const CONNECTION_URL =
-  'mongodb+srv://utask:utask123@cluster0.cr3nu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+  'mongodb+srv://root:admin@clusterutask.o0ouc.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 5000;
 
 mongoose
