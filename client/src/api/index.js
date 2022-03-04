1ae07d8c76ae48f68dcbd31cc8b3d2b3e0a59488
Victor Motta <victormotta7@gmail.com>
2022-03-03 22:31:32 -0300

    dispatch update(move)/delete

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -2,14 +2,8 @@ import axios from 'axios';
 
 const url = 'http://localhost:5000/tasks';
 
-<<<<<<< HEAD
 export const fetchTasks = () => axios.get(url);
 export const createTask = (newTask) => axios.post(url, newTask);
 export const updateTask = (id, updatedTask) =>
   axios.patch(`${url}/${id}`, updatedTask);
 export const deleteTask = (id) => axios.delete(`${url}/${id}`);
-=======
-export const fetchPosts = () => axios.get(url);
-export const createPost = (newPost) => axios.post(url, newPost);
-export const deletePost = (id) => axios.delte(`${url}/${id}`);
->>>>>>> 76a363c3602fd98d2aca2e499cbbcc4c20487336
--- client/src/constants/actionTypes.js
+++ client/src/constants/actionTypes.js
@@ -1,7 +1,4 @@
 export const FETCH_ALL = 'FETCH_ALL';
 export const CREATE = 'CREATE';
-<<<<<<< HEAD
 export const UPDATE = 'UPDATE';
-=======
->>>>>>> 76a363c3602fd98d2aca2e499cbbcc4c20487336
 export const DELETE = 'DELETE';
